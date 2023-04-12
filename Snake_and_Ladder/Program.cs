19b3c831468f6c14f536070d2275f23753fbafad
Prachi tote <prachi.tote77@gmail.com>
2023-04-12 22:07:34 +0530

    Game start with position 0

---
+++ Snake_and_Ladder/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+namespace Snake_And_Ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+            int Player_position = 0;
+            Console.WriteLine("Position = " + Player_position);
+
+        }
+    }
+}
