6d1cabe16d0fcbeab16779d14b74c016374122fa
Prachi tote <prachi.tote77@gmail.com>
2023-04-12 23:38:29 +0530

    Reporting each & evry Details

--- Snake_and_Ladder/Program.cs
+++ Snake_and_Ladder/Program.cs
@@ -12,11 +12,13 @@ namespace Snake_And_Ladder
             Console.WriteLine(" Snake and Ladder game played with single player at start position 0");
             int Player_position = 0;
             Console.WriteLine(" Position = " + Player_position);
+            int dice_roll = 0;
             Console.WriteLine(" The Player rolls the die to get a number between 1 to 6");
             Random random = new Random();
 
             while (Player_position < 100)
             {
+                dice_roll++;
                 int Die_num = random.Next(1, 7);
                 Console.WriteLine(" Number after rolling a die:");
                 Console.WriteLine($" {Die_num}");
@@ -42,14 +44,16 @@ namespace Snake_And_Ladder
                 if (Player_position < 0)
                 {
                     Player_position = 0;
-                    Console.WriteLine("Player is back to initial position = " + Player_position);
+
+                    Console.WriteLine("Player is back to initial = " + Player_position);
                 }
                 else if (Player_position > 100)
                 {
                     Player_position = Player_position - Die_num;
+                    Console.WriteLine("Player is back to initial position = " + Player_position);
                 }
             }
-
+            Console.WriteLine($" Number of times the dice was played to win the game = {dice_roll}");
         }
     }
 }
