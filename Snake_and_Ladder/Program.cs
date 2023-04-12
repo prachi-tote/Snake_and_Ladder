f02cae307c04438e6d38981997c44b749d27a9f4
Prachi tote <prachi.tote77@gmail.com>
2023-04-12 23:15:08 +0530

    Get number Between 1 to 6

--- Snake_and_Ladder/Program.cs
+++ Snake_and_Ladder/Program.cs
@@ -8,9 +8,15 @@ namespace Snake_And_Ladder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+            Console.WriteLine(" Snake and Ladder game played with single player at start position 0");
             int Player_position = 0;
-            Console.WriteLine("Position = " + Player_position);
+            Console.WriteLine(" Position = " + Player_position);
+            Console.WriteLine(" The Player rolls the die to get a number between 1 to 6");
+            Random random = new Random();
+            int Die_num = random.Next(1, 7);
+            Console.WriteLine(" Number after rolling a die:");
+            Console.WriteLine($" {Die_num}");
+
 
         }
     }
